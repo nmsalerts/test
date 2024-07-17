b6a8dfab2c3ac11fe86a27f0222a8f40a60dc255
nmsalerts <nms_alerts@iittp.ac.in>
2024-07-17 16:42:24 +0530

    Create stringdelimit.sh

---
+++ stringops/stringdelimit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Enter a string with commas: " user_input
+IFS=',' read -r -a array <<< "$user_input"
+
+echo "Splitted strings are:"
+for element in "${array[@]}"
+do
+    echo "$element"
+done
