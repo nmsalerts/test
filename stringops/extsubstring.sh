fd884b2fc80efc4e28107306c228e70620bdc508
nmsalerts <nms_alerts@iittp.ac.in>
2024-07-17 15:56:56 +0530

    Create extsubstring.sh

---
+++ stringops/extsubstring.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#!/bin/bash
+
+read -p "Enter a string: " user_input
+read -p "Enter the starting position: " start_pos
+read -p "Enter the length of the substring: " length
+
+substring=${user_input:start_pos:length}
+echo "Extracted substring is: $substring"
